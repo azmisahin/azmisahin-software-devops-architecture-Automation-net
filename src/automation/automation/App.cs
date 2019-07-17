3617bf4ea95bc3960c547a3b0824533347e48c0d
Azmi ŞAHİN <azmisahin@outlook.com>
2019-07-17 19:12:22 +0300

    Automation library started with "NET Framework 4".

---
+++ src/automation/automation/App.cs
@@ -0,0 +1,12 @@
+﻿namespace automation {
+    /// <summary>
+    /// Application
+    /// </summary>
+    public class App {
+
+        /// <summary>
+        /// Version Number
+        /// </summary>
+        public string Version { get; set; }
+    }
+}
